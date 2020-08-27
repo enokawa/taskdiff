354aabbe910bdfa2b18f87d0a4cf52d0f6e04e8f
enokawa <enokawa00@gmail.com>
2020-08-27 22:00:58 +0900

    implement validate args

--- main.go
+++ main.go
@@ -10,7 +10,22 @@ import (
 	"github.com/kylelemons/godebug/diff"
 )
 
+func validateArgs() error {
+	argLength := len(os.Args)
+	if argLength != 3 {
+		fmt.Println("error Argments.")
+		os.Exit(1)
+	}
+
+	return nil
+}
+
 func main() {
+	if err := validateArgs(); err != nil {
+		fmt.Println("validate error")
+		os.Exit(1)
+	}
+
 	args1 := os.Args[1]
 	args2 := os.Args[2]
 
